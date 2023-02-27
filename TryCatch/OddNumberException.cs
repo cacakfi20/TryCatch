3bec3b655672d4a0a3a97ecdb126f52d4b25318d
cacakfi20 <cacakfi20@sps-prosek.cz>
2023-02-27 13:16:44 +0100

    super more

---
+++ TryCatch/OddNumberException.cs
@@ -0,0 +1,31 @@
+﻿namespace TryCatch;
+    
+public class OddNumberException : Exception
+{
+    //Message
+    public override string Message
+    {
+        get
+        {
+            return "Error: Číslo je liché";
+        }
+    }
+    //HelpLink
+    public override string HelpLink
+    {
+        get
+        {
+            return "More Info: https://www.tutorialspoint.com/csharp/csharp_exception_handling.htm";
+        }
+    }
+    
+    //Source
+    public override string Source
+    {
+        get
+        {
+            return $"Source: {StackTrace.Split('\r').First().Trim()}";
+        }
+    }
+    
+}    
---
+++ TryCatch/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+
+// DĚLENÍ NULOU 
+
+bool program = true;
+
+while (program)
+{
+    int a = 1;
+    int b = 0;
+
+    try
+    {
+        double result = a/b;
+        Console.WriteLine($"{a} / {b} = {result}");
+    }
+    catch (DivideByZeroException e)
+    {
+        Console.WriteLine(e.Message + " (Bylo použito dělení nulou)");
+    }
+
+    break;
+
+}
+*/ 
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int i = 1;
+                if (i % 2 != 0)
+                {
+                    throw new OddNumberException();
+                }
+                else
+                {
+                    Console.WriteLine($"{i} je sudé");
+                }
+            }
+            catch (OddNumberException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.HelpLink);
+                Console.WriteLine(ex.Source);
+            }
+        }
+    }
+}
