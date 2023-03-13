19505d15fcb5aa873470f8e5798ad7145c3962fe
cacakfi20 <cacakfi20@sps-prosek.cz>
2023-03-13 13:12:26 +0100

    done

--- TryCatch/Program.cs
+++ TryCatch/Program.cs
@@ -1,31 +1,4 @@
-﻿
-/*
-
-// DĚLENÍ NULOU 
-
-bool program = true;
-
-while (program)
-{
-    int a = 1;
-    int b = 0;
-
-    try
-    {
-        double result = a/b;
-        Console.WriteLine($"{a} / {b} = {result}");
-    }
-    catch (DivideByZeroException e)
-    {
-        Console.WriteLine(e.Message + " (Bylo použito dělení nulou)");
-    }
-
-    break;
-
-}
-*/ 
-
-using System.Net;
+﻿using System.Net;
 namespace TryCatch
 {
     class Program
@@ -33,46 +6,8 @@ namespace TryCatch
         
         static void Main()
         {
-            /*
-            try
-            {
-                int i = 1;
-                if (i % 2 != 0)
-                {
-                    throw new OddNumberException();
-                }
-                else
-                {
-                    Console.WriteLine($"{i} je sudé");
-                }
-            }
-            catch (OddNumberException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.HelpLink);
-                Console.WriteLine(ex.Source);
-            }
-*/
-            
-            // http = 80, https = 443, ftp = 21
-            /*string online = Serveronline.ServerActive(url, 443);
-            Console.WriteLine(online);
-            */
-            //
-            
-            string url = "https://www.youtube.com/";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Method = "HEAD";
-            request.AllowAutoRedirect = false;
-            try
-            {
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                Console.WriteLine("Server status: " + response.StatusCode);
-            }
-            catch (WebException ex)
-            {
-                Console.WriteLine("Error connecting to server: " + ex.Status);
-            }
+            Server_diagnos srv = new Server_diagnos();
+            srv.repeat_log("https://http.cat/201");
         }
     }
 }
---
+++ TryCatch/Server_diagnos.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+namespace TryCatch;
+
+public class Server_diagnos
+{
+    public string url { get; set; }
+
+    public string GetStatus(string url)
+    {
+        string ret;
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.Method = "HEAD";
+        request.AllowAutoRedirect = false;
+        try
+        {
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            ret = url + " status: " + (int)response.StatusCode + " - " + response.StatusCode;
+        }
+        catch (WebException ex)
+        {
+            if (ex.Status == WebExceptionStatus.ProtocolError)
+            {
+                var response = ex.Response as HttpWebResponse;
+                if (response != null)
+                {
+                    ret = "Error connecting to " + url + ": " + (int)response.StatusCode + " - " + ex.Status;
+                }
+                else
+                {
+                    ret = "Error connecting to " + url + ": " + ex.Status;
+                }
+            }
+            else
+            {
+                ret = "Error connecting to " + url + ": " + ex.Status;
+            }
+            
+        }
+
+        return ret;
+    }
+
+    //loguje každých 5 sekundy
+    public void repeat_log(string url)
+    {
+        while (true)
+        {
+            Console.WriteLine(GetStatus(url));
+            Thread.Sleep(5000);
+        }
+    }
+}
